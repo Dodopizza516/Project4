ceaf73aa3be94f72531ab63acc1a873afd541ce3
dodopizza516 <danila.tarasov.2025@mail.ru>
2026-02-11 19:01:06 +0300

    test

--- Project4/MyForm.h
+++ Project4/MyForm.h
@@ -57,6 +57,12 @@ namespace Project4 {
 	private: System::Windows::Forms::CheckBox^ checkBox3;
 	private: System::Windows::Forms::CheckBox^ checkBox2;
 	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
 
 	private:
 		/// <summary>
@@ -73,6 +79,11 @@ namespace Project4 {
 		{
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->checkBox4 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage3 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage4 = (gcnew System::Windows::Forms::TabPage());
@@ -83,13 +94,20 @@ namespace Project4 {
 			this->tabPage9 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage10 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage11 = (gcnew System::Windows::Forms::TabPage());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox4 = (gcnew System::Windows::Forms::CheckBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->tabControl1->SuspendLayout();
 			this->tabPage1->SuspendLayout();
+			this->tabPage2->SuspendLayout();
+			this->tabPage3->SuspendLayout();
+			this->tabPage4->SuspendLayout();
+			this->tabPage5->SuspendLayout();
+			this->tabPage6->SuspendLayout();
+			this->tabPage7->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// tabControl1
@@ -126,8 +144,59 @@ namespace Project4 {
 			this->tabPage1->Text = L"Вопрос 1";
 			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
+			// checkBox4
+			// 
+			this->checkBox4->AutoSize = true;
+			this->checkBox4->Location = System::Drawing::Point(28, 160);
+			this->checkBox4->Name = L"checkBox4";
+			this->checkBox4->Size = System::Drawing::Size(80, 17);
+			this->checkBox4->TabIndex = 4;
+			this->checkBox4->Text = L"checkBox4";
+			this->checkBox4->UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this->checkBox3->AutoSize = true;
+			this->checkBox3->Location = System::Drawing::Point(28, 124);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(80, 17);
+			this->checkBox3->TabIndex = 3;
+			this->checkBox3->Text = L"checkBox3";
+			this->checkBox3->UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(28, 92);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(80, 17);
+			this->checkBox2->TabIndex = 2;
+			this->checkBox2->Text = L"checkBox2";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(28, 60);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(80, 17);
+			this->checkBox1->TabIndex = 1;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9));
+			this->label1->Location = System::Drawing::Point(6, 22);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(269, 15);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Сколько смешариков было в первых сериях\?";
+			// 
 			// tabPage2
 			// 
+			this->tabPage2->Controls->Add(this->label2);
 			this->tabPage2->Location = System::Drawing::Point(4, 22);
 			this->tabPage2->Name = L"tabPage2";
 			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
@@ -139,6 +208,7 @@ namespace Project4 {
 			// 
 			// tabPage3
 			// 
+			this->tabPage3->Controls->Add(this->label3);
 			this->tabPage3->Location = System::Drawing::Point(4, 22);
 			this->tabPage3->Name = L"tabPage3";
 			this->tabPage3->Size = System::Drawing::Size(294, 311);
@@ -148,6 +218,7 @@ namespace Project4 {
 			// 
 			// tabPage4
 			// 
+			this->tabPage4->Controls->Add(this->label4);
 			this->tabPage4->Location = System::Drawing::Point(4, 22);
 			this->tabPage4->Name = L"tabPage4";
 			this->tabPage4->Size = System::Drawing::Size(294, 311);
@@ -157,6 +228,7 @@ namespace Project4 {
 			// 
 			// tabPage5
 			// 
+			this->tabPage5->Controls->Add(this->label5);
 			this->tabPage5->Location = System::Drawing::Point(4, 22);
 			this->tabPage5->Name = L"tabPage5";
 			this->tabPage5->Size = System::Drawing::Size(294, 311);
@@ -166,6 +238,7 @@ namespace Project4 {
 			// 
 			// tabPage6
 			// 
+			this->tabPage6->Controls->Add(this->label6);
 			this->tabPage6->Location = System::Drawing::Point(4, 22);
 			this->tabPage6->Name = L"tabPage6";
 			this->tabPage6->Size = System::Drawing::Size(294, 311);
@@ -175,6 +248,7 @@ namespace Project4 {
 			// 
 			// tabPage7
 			// 
+			this->tabPage7->Controls->Add(this->label7);
 			this->tabPage7->Location = System::Drawing::Point(4, 22);
 			this->tabPage7->Name = L"tabPage7";
 			this->tabPage7->Size = System::Drawing::Size(294, 311);
@@ -218,55 +292,71 @@ namespace Project4 {
 			this->tabPage11->Text = L"Результат";
 			this->tabPage11->UseVisualStyleBackColor = true;
 			// 
-			// label1
+			// label2
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9));
-			this->label1->Location = System::Drawing::Point(25, 22);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(130, 15);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Какого цвета трава\?";
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9));
+			this->label2->Location = System::Drawing::Point(6, 22);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(254, 30);
+			this->label2->TabIndex = 0;
+			this->label2->Tag = L"";
+			this->label2->Text = L"Известно что  Сергей Мардарь озвучивал \r\nдвух персонажей, вопрос каких\?";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
 			// 
-			// checkBox1
+			// label3
 			// 
-			this->checkBox1->AutoSize = true;
-			this->checkBox1->Location = System::Drawing::Point(28, 60);
-			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(80, 17);
-			this->checkBox1->TabIndex = 1;
-			this->checkBox1->Text = L"checkBox1";
-			this->checkBox1->UseVisualStyleBackColor = true;
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9));
+			this->label3->Location = System::Drawing::Point(3, 22);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(267, 30);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Сколько смешариков носят головные уборы \r\nна постоянной основе\?";
 			// 
-			// checkBox2
+			// label4
 			// 
-			this->checkBox2->AutoSize = true;
-			this->checkBox2->Location = System::Drawing::Point(28, 92);
-			this->checkBox2->Name = L"checkBox2";
-			this->checkBox2->Size = System::Drawing::Size(80, 17);
-			this->checkBox2->TabIndex = 2;
-			this->checkBox2->Text = L"checkBox2";
-			this->checkBox2->UseVisualStyleBackColor = true;
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(3, 29);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(182, 15);
+			this->label4->TabIndex = 0;
+			this->label4->Text = L"Что коллекционировал Ежик\?";
 			// 
-			// checkBox3
+			// label5
 			// 
-			this->checkBox3->AutoSize = true;
-			this->checkBox3->Location = System::Drawing::Point(28, 124);
-			this->checkBox3->Name = L"checkBox3";
-			this->checkBox3->Size = System::Drawing::Size(80, 17);
-			this->checkBox3->TabIndex = 3;
-			this->checkBox3->Text = L"checkBox3";
-			this->checkBox3->UseVisualStyleBackColor = true;
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(3, 29);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(137, 15);
+			this->label5->TabIndex = 0;
+			this->label5->Text = L"Как звали сына Пина\?";
 			// 
-			// checkBox4
+			// label6
 			// 
-			this->checkBox4->AutoSize = true;
-			this->checkBox4->Location = System::Drawing::Point(28, 160);
-			this->checkBox4->Name = L"checkBox4";
-			this->checkBox4->Size = System::Drawing::Size(80, 17);
-			this->checkBox4->TabIndex = 4;
-			this->checkBox4->Text = L"checkBox4";
-			this->checkBox4->UseVisualStyleBackColor = true;
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(3, 27);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(261, 30);
+			this->label6->TabIndex = 0;
+			this->label6->Text = L"На каком музыкальном инструменте играл \r\nКар-Карыч\?";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label7->Location = System::Drawing::Point(60, 57);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(41, 15);
+			this->label7->TabIndex = 0;
+			this->label7->Text = L"label7";
 			// 
 			// MyForm
 			// 
@@ -280,6 +370,18 @@ namespace Project4 {
 			this->tabControl1->ResumeLayout(false);
 			this->tabPage1->ResumeLayout(false);
 			this->tabPage1->PerformLayout();
+			this->tabPage2->ResumeLayout(false);
+			this->tabPage2->PerformLayout();
+			this->tabPage3->ResumeLayout(false);
+			this->tabPage3->PerformLayout();
+			this->tabPage4->ResumeLayout(false);
+			this->tabPage4->PerformLayout();
+			this->tabPage5->ResumeLayout(false);
+			this->tabPage5->PerformLayout();
+			this->tabPage6->ResumeLayout(false);
+			this->tabPage6->PerformLayout();
+			this->tabPage7->ResumeLayout(false);
+			this->tabPage7->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
@@ -287,6 +389,8 @@ namespace Project4 {
 	private: System::Void tabPage2_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
