8018bf7aee13ec83479c63fcd13676b97901e1d2
Тарасов Данила <danila.tarasov.2025@mail.ru>
2026-02-20 12:01:38 +0300

    test

--- Project4/MyForm.cpp
+++ Project4/MyForm.cpp
@@ -1,2 +1,11 @@
 #include "MyForm.h"
+#include <Windows.h>
 
+// Убираем using namespace Project4;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MyForm());  // Обратите внимание на скобки после MyForm
+    return 0;
+}
